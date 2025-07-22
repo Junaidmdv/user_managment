7b4c4f2599d57243d4a835e79c666321e9ba54a3
Junaidmdv <juni313786@gmail.com>
2025-07-22 08:58:52 +0530

    add changes in config

--- internal/config/viper.go
+++ internal/config/viper.go
@@ -2,27 +2,18 @@ package config
 
 import (
 	"log"
-	"os"
 
-	"github.com/joho/godotenv"
 	"github.com/spf13/viper"
 )
 
 func LoadConfig() *viper.Viper {
 	v := viper.New()
-
-	err := godotenv.Load("CONFIG_PATH")
-
-	if err != nil {
-		log.Fatalf("CONFIG_PATH environment variable not set %v ", err)
-	}
-
 	v.SetConfigName("config")
 	v.SetConfigType("env")
-	v.AddConfigPath(os.Getenv("CONFIG_PATH"))
+	v.AddConfigPath("/home/junaid/user_mangment/")
 
 	if err := v.ReadInConfig(); err != nil {
-		log.Fatalf("Error reading config file: %v", err)
+		log.Fatal("Failed to load config file: ", err)
 	}
 
 	return v

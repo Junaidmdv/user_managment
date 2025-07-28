4c5339023843560f8a43491cdd547d09570d7f83
Junaidmdv <juni313786@gmail.com>
2025-07-28 23:45:31 +0530

    made changes in  viper config

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,17 +1,28 @@
 package main
 
 import (
+	"log"
+
 	"github.com/junaidmdv/user_mangment/internal/config"
+	"github.com/spf13/viper"
 )
 
 func main() {
-	viper := config.LoadConfig()
-	db := config.DbConnection(viper)
+	v := viper.New()
+	v.SetConfigName("config")
+	v.SetConfigType("env")
+	v.AddConfigPath("../../")
+
+	if err := v.ReadInConfig(); err != nil {
+		log.Fatal("Failed to load config file: ", err)
+	}
+
+	db := config.DbConnection(v)
 	config.Migrate(db)
 	app := config.NewServer()
 
 	config.Bootstrap(&config.BootstrapConfig{
-		Viper: viper,
+		Viper: v,
 		DB:    db,
 		App:   app,
 	})

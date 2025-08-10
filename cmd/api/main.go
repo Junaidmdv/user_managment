65dca82933e1a470f26393c9d59bf767b79958ff
Junaidmdv <juni313786@gmail.com>
2025-08-11 00:14:54 +0530

    Print message deleted

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 
 	"github.com/junaidmdv/user_mangment/internal/config"
@@ -9,7 +8,6 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello world")
 	v := viper.New()
 	v.SetConfigName("config")
 	v.SetConfigType("env")
